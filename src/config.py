df46fe2ffbad5be009654c76c6826197d505df17
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-29 17:46:21 +0300

    Add configuration variables for Rocket.Chat bot

--- src/config.py
+++ src/config.py
@@ -1,1 +1,7 @@
 # Configuration variables (URLs, credentials, etc.)
+import os
+
+ROCKETCHAT_URL = os.getenv("ROCKETCHAT_URL")
+ROCKETCHAT_USER = os.getenv("ROCKETCHAT_USER")
+ROCKETCHAT_PASSWORD = os.getenv("ROCKETCHAT_PASSWORD")
+GREET_CHANNEL = os.getenv("GREET_CHANNEL", "#general")
