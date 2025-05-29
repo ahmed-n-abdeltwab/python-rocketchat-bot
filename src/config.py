3e50c57e3c0da689dd8f83ee41e2c8bcd88ca0fd
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-29 18:30:14 +0300

    fix: Update ROCKETCHAT_USER and ROCKETCHAT_PASSWORD for consistency in configuration

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@
 import os
 
 ROCKETCHAT_URL = os.getenv("ROCKETCHAT_URL", "http://localhost:3000")
-ROCKETCHAT_USER = os.getenv("ROCKETCHAT_USER", "bot")
-ROCKETCHAT_PASSWORD = os.getenv("ROCKETCHAT_PASSWORD", "botpassword")
+ROCKETCHAT_USER = os.getenv("ROCKETCHAT_USER", "rocketchat.internal.admin.test")
+ROCKETCHAT_PASSWORD = os.getenv("ROCKETCHAT_PASSWORD", "rocketchat.internal.admin.test")
 GREET_CHANNEL = os.getenv("GREET_CHANNEL", "general")
