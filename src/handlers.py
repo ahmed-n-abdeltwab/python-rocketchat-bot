ad1d8e19243c915870bbe7353ff5d26855fae635
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2025-05-30 17:24:38 +0300

    fix: Refactor user management functions to improve error handling and consistency

--- src/handlers.py
+++ src/handlers.py
@@ -5,13 +5,49 @@ import config
 client = RocketChatClient()
 
 
+def connect():
+    """
+    Connect to the RocketChat server.
+    """
+    client.connect()
+
+
+def logout():
+    """
+    Logout from the RocketChat server.
+    """
+    if not client.is_connected():
+        raise RuntimeError("RocketChat client is not connected. Call connect() first.")
+
+    try:
+        client.logout()
+        print("Logged out successfully")
+    except Exception as e:
+        print(f"Failed to logout: {str(e)}")
+        raise
+
+
+def get_users(**kwargs) -> list:
+    """
+    Retrieve all users and their information, limited to permissions.
+    """
+    if not client.is_connected():
+        raise RuntimeError("RocketChat client is not connected. Call connect() first.")
+
+    try:
+        return client.get_users(**kwargs)
+    except Exception as e:
+        print(f"Failed to get the users: {str(e)}")
+        raise
+
+
 def greet_login(user: str):
     """
     Send a welcome message to the greet channel when a user logs in.
     """
     message = f"Welcome, @{user}! Glad you're here! 🚀"
-    room_id = client.get_room_id(room_name=config.GREET_CHANNEL)
-    client.send_message(message=message, room=room_id)
+    room_id = client.get_room_id(room_name=config.CHANNEL)
+    client.send_message(text=message, room_id=room_id)
 
 
 def greet_logout(user: str):
@@ -19,5 +55,5 @@ def greet_logout(user: str):
     Send a goodbye message to the greet channel when a user logs out.
     """
     message = f"See you later, @{user}! 👋"
-    room_id = client.get_room_id(config.GREET_CHANNEL)
-    client.send_message(message, room_id)
+    room_id = client.get_room_id(room_name=config.CHANNEL)
+    client.send_message(text=message, room_id=room_id)
